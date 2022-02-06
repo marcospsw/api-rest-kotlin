239f9767cd9bbad9af012cf276a66036ff9d36ca
Marcos Wergles <marcospsw96@gmail.com>
2022-02-06 20:22:35 -0300

    feat: estudos kotlin #000

---
+++ src/main/kotlin/com/donus/restapiforum/RestApiForumApplication.kt
@@ -0,0 +1,11 @@
+package com.donus.restapiforum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiForumApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApiForumApplication>(*args)
+}
